e81994bdfe8ff952f33d3fabd22129699f1cd1f0
aditore <anthonyditore@gmail.com>
2022-03-12 11:39:40 -0800

    all roles and seeds have been added to database

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,13 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
---tables--
---department--
+USE employee_db;
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
---roles--
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
@@ -17,7 +16,6 @@ CREATE TABLE role (
     FOREIGN KEY (departmentId) REFERENCES department(id)
 );
 
---employees--
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(30),
